5356044068549977045c5d41d94ff3e90f30f393
Lina Muñoz <linamu24@gmail.com>
2025-02-07 09:12:04 -0500

    pre

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "bd1",
+        "USER": "linam",
+        "PASSWORD": "Li24102004na",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
